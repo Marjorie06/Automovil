a0506cc31065b8c7d913a17918e7b9964d9e0390
Marjorie06 <marjoriechal06@gmail.com>
2024-11-29 16:16:38 -0500

    primera prueba

---
+++ Automovil1/src/main/java/ec/edu/espoch/automovil1/Automovil1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.automovil1;
+
+/**
+ *
+ * @author SO-LAB1-PC13
+ */
+public class Automovil1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    
+}
