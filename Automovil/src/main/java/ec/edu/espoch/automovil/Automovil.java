b6a3f849ce1ae1372d57de51333a35f42cfb6755
kar-gl <karenanaluisa40@gmail.com>
2024-11-29 16:29:56 -0500

    rama clase

---
+++ Automovil/src/main/java/ec/edu/espoch/automovil/Automovil.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.automovil;
+
+/**
+ *
+ * @author SO-LAB1-PC14
+ */
+public class Automovil {
+
+    public static void main(String[] args) {
+       
+        System.out.println("rama");
+}
+}
